8166b689742175d17e254ffd509ad32f8612068d
shohaibMahmud <shohaib.mahmud@gmail.com>
2023-06-21 13:49:05 -0400

    Add all functionalities

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # calculator
+This is a simple calculator implemented as the finaly project in the foundation course of TOP. In addition to the project's functionality, a minidisplay for the previous operation and a sign button were added.
+
+1. Maximum length an operand can have is 28. Since the calculator takes input beyond the safe value supported by JS, results could be inaccurate.
+
+2. If the minidisplay reaches its character limit, the display will freeze and show three dots "..." at the end to specify some operations have been clipped at the end.
+
+3. Any attempt to input a number before specifying an operation will erase an existing chain of operations. If there is no chain, there will be no effect.
+
+4. Rounding functionality was not tested as JS loses precision beyond the safe value.
--- js-file.js
+++ js-file.js
@@ -1,13 +1,13 @@
+const MAIN_DIS_CHAR_LIM = 28;
+const MINI_DIS_CHAR_LIM = 56;
 const mainDisplay = document.querySelector("#main-display");
 const miniDisplay = document.querySelector("#mini-display");
 
-let operands = ["0", "0"];
-let currentOperand = 0;
-let operate = null;
-let operandResult = false;
-let operandFloat = false;
-
-clear();
+let operands = ["0", "0"];  // Operands value set to 0 as default
+let currentOperand = 0;     // Denotes first or second operand
+let operate = null;        // reference to add, mul... function
+let operandResult = false; //denotes operand 0 is from previous result
+let operandFloat = false; //denotes current operand is a float
 
 function clear(){
     operands[0] = "0";
@@ -20,9 +20,20 @@ function clear(){
     miniDisplay.textContent = "";
 }
 
+function updateMiniDisplay(text){
+    if ((miniDisplay.textContent.length+text.length)<MINI_DIS_CHAR_LIM){
+        miniDisplay.textContent += text;
+    }
+    else{
+        const temp = (miniDisplay.textContent+text).slice(0, MINI_DIS_CHAR_LIM-3)+"...";
+        miniDisplay.textContent = temp;
+    }
+}
+
 function evaluate(){
     if (operate){
-        miniDisplay.textContent += ` ${operands[currentOperand]}`;
+        //miniDisplay.textContent += ` ${operands[currentOperand]}`;
+        updateMiniDisplay(` ${operands[currentOperand]}`)
         const a = Number(operands[0]);
         const b = Number(operands[1]);
         if (isNaN(a) || isNaN(b)){
@@ -30,7 +41,12 @@ function evaluate(){
             mainDisplay.textContent = "Invalid operand(s)";
         }
         else{
-            const res = operate(a, b)
+            let res = operate(a, b);
+            if (res.length > MAIN_DIS_CHAR_LIM){   //Rounding number to fit display. Not tested thoroughly
+                const decim_chars = res.indexOf(".");
+                if (decim_chars == -1) res = res.slice(0, MAIN_DIS_CHAR_LIM);
+                else res = Number(res).toFixed(MAIN_DIS_CHAR_LIM-decim_chars);
+            }
             mainDisplay.textContent = res;
             operands[0] = res;
             operands[1] = "0"
@@ -42,26 +58,27 @@ function evaluate(){
 }
 
 function add(a,b){
-    return (a+b).toString();
+    return (a+b).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 20});
 }
 function subtract(a,b){
-    return (a-b).toString();
+    return (a-b).toLocaleString({minimumFractionDigits: 0, maximumFractionDigits: 20});
 }
 function multiply(a,b){
-    return (a*b).toString();
+    return (a*b).toLocaleString({minimumFractionDigits: 0, maximumFractionDigits: 20});
 }
 function divide(a,b){
-    if (b == 0) return "Indeterminate";
-    else return (a/b).toString();
+    if (a==0 && b == 0) return "Undefined";
+    else if (b == 0) return "Indeterminate";
+    else return (a/b).toLocaleString({minimumFractionDigits: 0, maximumFractionDigits: 20});
 }
 
 function setOperator(e){
     if (operate) evaluate();
     
     const operator = e.target.getAttribute("id");
-    if (operandResult) miniDisplay.textContent += ` ${operator}`;
+    if (operandResult) updateMiniDisplay(` ${operator}`);
     else{
-        miniDisplay.textContent += `${operands[currentOperand]} ${operator}`;
+        updateMiniDisplay(`${operands[currentOperand]} ${operator}`);
         currentOperand += 1;
         mainDisplay.textContent = operands[currentOperand];
     }
@@ -82,13 +99,14 @@ function updateOperand(e){
         operands[currentOperand] = digit;
         mainDisplay.textContent = digit;
     }
-    else{
+    else if(operands[currentOperand].length<MAIN_DIS_CHAR_LIM){
         operands[currentOperand] += digit;
         mainDisplay.textContent += digit;
     }
 }
 
 function toggleSign(){
+    if (operandResult) clear();
     if (operands[currentOperand].charAt(0) == "-")
         operands[currentOperand] = operands[currentOperand].slice(1, operands[currentOperand].length);
     else if (operands[currentOperand] != "0")
@@ -97,9 +115,10 @@ function toggleSign(){
 }
 
 function makeFloat(){
+    if (operandResult) clear();
     if (!operandFloat){
         operands[currentOperand] += ".";
-        mainDisplay.textContent += ".";
+        mainDisplay.textContent = operands[currentOperand];
     }
 }
 
@@ -116,7 +135,7 @@ function backSpace(){
         mainDisplay.textContent = operands[currentOperand];
     }
 }
-
+clear();
 document.querySelector("#clear").addEventListener("click", clear)
 
 document.querySelectorAll(".number").forEach(button=>{
